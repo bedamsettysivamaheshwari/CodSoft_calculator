b3ccef254f510a4d347bfcbc38debc31a6f32518
bedamsettysivamaheshwari <bedamsettysivamaheshwari@gmail.com>
2026-06-16 20:12:00 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def add(x,y):
+  return x + y
+
+def subtract(x,y):
+  return x - y
+
+def multiply(x,y):
+  return x * y
+
+def divide(x,y):
+  if y == 0:
+    return "Error: Divison by zero!"
+  return x / y
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+operation = input("choose operation(+ ,- ,* ,/):")
+
+if operation == '+' :
+  print("Result:" , add(num1,num2))
+elif operation == '-' :
+  print("Result:" , subtract(num1,num2))
+elif operation == '*' :
+  print("Result:" , multiply(num1,num2))
+elif operation == '/' :
+  print("Result:" , divide(num1,num2))
+else:
+  print("Invalid input")
